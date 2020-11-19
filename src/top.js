0a502204bcc9a24008002f538ae7843496c0c682
kimutai-codes <allankimutaikoech@gmail.com>
2020-11-19 22:47:02 +0300

    first commit

---
+++ src/top.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// function container (props) {
+// 	return <h1>Ssup {props.name}</h1>;
+// };
+
+const container = (props) => {
+	return (
+		<p className="top-part">
+			Kimutai codes @kimutai-codes
+			<br />
+			"Starting Small" sucks but that's the way
+			<br />
+			{'{author:@allano}'}
+		</p>
+	);
+};
+
+export default container;
